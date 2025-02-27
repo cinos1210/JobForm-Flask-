4baaad948d67826f6bce7ef98211a594e339607a
RAFAELARTUROGUTIERRE <rafael.gutierrez4666@alumnos.udg.mx>
2025-02-27 05:13:28 -0600

    Adding email sending to user

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_mail import Mail, Message
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "cinosonic159487"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = "rafael.gutierrez4666@alumnos.udg.mx"
+app.config["MAIL_PASSWORD"] = "srlssvmbxshpdldi"
+
 db = SQLAlchemy(app)
+
+mail = Mail(app)
 
 class Form(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -31,6 +40,18 @@ def index():
 
         db.session.add(form)
         db.session.commit()
+
+        message_body = (f"Thank you for your submission, {first_name}."
+                        f"Here are your data:\n{first_name}\n{last_name}\n{date}\n{occupation}")
+
+        message = Message(subject="New form submission",
+                          sender=app.config["MAIL_USERNAME"],
+                          recipients=[email],
+                          body=message_body)
+
+        mail.send(message)
+
+        flash(f"{first_name}, Your form was submitted successfully", "success")
 
     return render_template("index.html")
 
